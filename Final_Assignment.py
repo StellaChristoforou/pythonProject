085e82b7e934e3ee0fe43fd4dfd8859e4f4515bd
stellachristoforou98 <stella_christoforou98@outlook.com>
2023-03-13 10:46:35 +0200

    This is my Final Assignment!

--- Final_Assignment.py
+++ Final_Assignment.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 
 data=pd.read_csv("liquor_sales_FinalAssignment1.csv")
 
-'''
 x=data.groupby(["zip_code","item_description"]).bottles_sold.max()
-print(x)
-'''
+print("Most Famous Item per Zip Code\n==============================\n")
+print(x,"\n\n\n")
+
 y=data.groupby('store_name').sale_dollars.sum()
 sum_sales=data['sale_dollars'].sum()
 
 percentage=(y/sum_sales)*100
-
+print("Percentage (%) of Sales per Store\n==================================\n")
+print(round(percentage,2),"\n\n\n")
 
 store_names=set(data['store_name'])
 s=list(store_names)
@@ -21,7 +22,7 @@ sn_sort=np.sort(sn)
 
 plt.subplot(2,1,1)
 plt.pie(percentage,labels=sn_sort)
-
+plt.title("Percentage of Sales per Store")
 z=data.groupby('zip_code').bottles_sold.sum()
 
 
@@ -32,6 +33,7 @@ zipcode=np.sort(zip_codes_array)
 
 plt.subplot(2,1,2)
 plt.scatter(zipcode, z, color=np.random.rand(len(zipcode),3))
+plt.title("Bottles Sold per Zip Code")
 plt.xlabel("Zip Code")
 plt.ylabel("Bottles Sold")
 plt.show()
